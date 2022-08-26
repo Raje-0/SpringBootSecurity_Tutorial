03dee0702cb71de86f1d9900b83c9bc72c124bec
kanhaiya kumar <97101583+Raje-0@users.noreply.github.com>
2022-08-26 17:12:41 +0530

    initial commit

---
+++ src/main/java/com/learn/springbootsecuritytutotial/SpringBootSecurityTutotialApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springbootsecuritytutotial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityTutotialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityTutotialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/springbootsecuritytutotial/SpringBootSecurityTutotialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springbootsecuritytutotial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityTutotialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
